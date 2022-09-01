de31ba7188f2f32a88237cc179c40b92b1e268cf
Sana2786 <61844429+Sana2786@users.noreply.github.com>
2022-09-02 00:09:20 +0530

    Create 2367. Number of Arithmetic Triplets.java

---
+++ 2367. Number of Arithmetic Triplets.java
@@ -0,0 +1,16 @@
+#Runtime: 0 ms, faster than 100.00% of Java online submissions for Number of Arithmetic Triplets.
+class Solution {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        final int kMax = 200;
+    int ans = 0;
+    boolean[] count = new boolean[kMax + 1];
+
+    for (final int num : nums) {
+      if (num >= 2 * diff && count[num - diff] && count[num - 2 * diff])
+        ++ans;
+      count[num] = true;
+    }
+
+    return ans;
+    }
+}
